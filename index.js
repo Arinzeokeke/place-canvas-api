b860c3a8ca5acabb435996d744ad02eb947b0eb8
Arinze Okeke <trent9002@gmail.com>
2017-11-06 16:56:43 +0100

    set up express

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const keys = require('./config/keys')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+mongoose.connect(keys.mongoUrl)
+
+if (isProduction) {
+  mongoose.set('debug', true)
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server listen on port ${process.env.PORT || 3000}`)
