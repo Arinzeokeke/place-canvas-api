ce71eebf02f7135a789b356b8a1876489e37c5b7
Arinze Okeke <trent9002@gmail.com>
2017-11-09 15:20:42 +0100

    add prod

---
+++ config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongoUrl: process.env.MONGO_URL,
+  secret: process.env.SECRET,
+  facebookClientId: process.env.FACEBOOK_CLIENT_ID,
+  facebookSecretKey: process.env.FACEBOOK_SECRET_KEY,
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET
+}
