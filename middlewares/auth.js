0e306eceba62064785708339c0c60f0a699e2223
Arinze Okeke <trent9002@gmail.com>
2017-11-09 08:37:37 +0100

    login endpoint

---
+++ middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('express-jwt')
+const secret = require('../config/keys').secret
+
+const getTokenFromHeader = req => {
+  const splitHeader = req.headers.authorization.split(' ')
+  if (
+    req.headers.authorization &&
+    splitHeader.length === 2 &&
+    splitHeader[0].toLowerCase() === 'bearer'
+  ) {
+    return splitHeader[1]
+  }
+  return null
+}
+
+module.exports = jwt({
+  secret: secret,
+  userProperty: 'payload',
+  getToken: getTokenFromHeader
+})
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const validations = require('../../middlewares/validations')
 
 router.post('/users', validations, async (req, res) => {
@@ -10,5 +11,20 @@ router.post('/users', validations, async (req, res) => {
 
   const newUser = await user.save()
 
-  return res.json({ user: user.generateAuthBody() })
+  return res.send({ user: user.generateAuthBody() })
 })
+
+router.post('/users/login', validations, (req, res, next) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+    if (user) {
+      return res.send({ user: user.generateAuthBody() })
+    } else {
+      return res.status(422).json(info)
+    }
+  })(req, res, next)
+})
+
+module.exports = router
